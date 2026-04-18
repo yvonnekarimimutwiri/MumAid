69eb719c1a06227eb3d0059f64b50e3ba36f5ce9
n.ull10codes <gmutuku05@gmail.com>
2026-04-18 15:13:04 +0300

    Initialized with expo, and set up nativewind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+export default function Index() {
+	return (
+		<View className="flex-1 items-center justify-center">
+			<Text className="text-xl font-bold text-blue-500">
+				Edit app/index.tsx to edit this screen.
+			</Text>
+		</View>
+	)
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.tsx",
+		"./app/**/*.{js,jsx,ts,tsx}",
+		"./components/**/*.{js,jsx,ts,tsx}",],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
