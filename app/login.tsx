8a7a6d539cb94b74e030cde8ab36c3602b4b7ba9
n.ull10codes <gmutuku05@gmail.com>
2026-04-25 15:32:27 +0300

    URL implementation

---
+++ app/login.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { API_URL } from '../config/api'
+
+const login = () => {
+  return (
+    <View>
+      <Text>login</Text>
+    </View>
+  )
+}
+
+export default login
+
+const styles = StyleSheet.create({})
---
+++ constants/Config.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://momaid-backend.onrender.com/api"
